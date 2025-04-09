095131c8183b10412b424255ff5b993651762dda
alhabib <mohalshaibali@gmail.com>
2025-04-09 22:30:51 +0300

    login page layout

--- frontend/src/logIn.js
+++ frontend/src/logIn.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import './Login.css';
-import logo from './assets/logo.png';
+import leftLogo from './assets/logo-left.png';
+import rightLogo from './assets/logo-right.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
   return (
     <div className="login-wrapper">
-      {/* Background section */}
-      <div className="login-background"></div>
-
-      {/* Overlay section */}
-      <div className="login-overlay">
-        <div className="login-content">
-          {/* Logo */}
-          <img src={logo} alt="Customs Logo" className="login-logo" />
-
-          {/* Title */}
-          <h1 className="login-title">SenseAI</h1>
-
-          {/* Form */}
-          <form className="login-form">
-            <div className="input-group">
-              <FontAwesomeIcon icon={faUser} className="input-icon" />
-              <input type="email" placeholder="Email" />
-            </div>
-
-            <div className="input-group">
-              <FontAwesomeIcon icon={faLock} className="input-icon" />
-              <input type="password" placeholder="Password" />
-            </div>
-
-            <div className="login-extra">
-              <a href="/">Forget password ?</a>
-            </div>
-
-            <button type="submit">Enter</button>
-          </form>
+      <div className="login-header">
+        <img src={leftLogo} alt="Bahrain Logo" className="logo" />
+        <div className="header-text">
+          <div>KINGDOM OF BAHRAIN</div>
+          <div>MINISTRY OF INTERIOR</div>
+          <div>CUSTOMS AFFAIRS</div>
         </div>
+        <img src={rightLogo} alt="Customs Logo" className="logo" />
       </div>
+
+      <div className="title-container">
+        <h1 className="senseai-title">SenseAI</h1>
+        <p className="login-subtitle">Login</p>
+      </div>
+
+      <form className="login-form">
+        <h2>Sign in to your account</h2>
+
+        <div className="input-group">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <input type="email" placeholder="Email address" />
+        </div>
+
+        <div className="input-group">
+          <FontAwesomeIcon icon={faLock} />
+          <input type="password" placeholder="Password" />
+        </div>
+
+        <div className="login-extra">
+          <a href="/">Forget password?</a>
+        </div>
+
+        <button type="submit">Sign in</button>
+      </form>
     </div>
   );
 }
