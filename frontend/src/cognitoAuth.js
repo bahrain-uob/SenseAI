c53ef9728902da7bc6bf64fb303ce4f5d0adf531
alhabib <mohalshaibali@gmail.com>
2025-04-15 23:49:48 +0300

    fixedd

--- frontend/src/cognitoAuth.js
+++ frontend/src/cognitoAuth.js
@@ -5,9 +5,10 @@ import {
 } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'me-south-1_h4hXEsiSh', 
-  ClientId: '4o8u68vk9loi71g7afpno9m9m5' 
+  UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
 };
+
 
 const userPool = new CognitoUserPool(poolData);
 
